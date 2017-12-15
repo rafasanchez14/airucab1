02281f8726cf1885e28cf5d4f04cb6d4ec2b3dba
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-14 20:50:10 -0430

    sin cambios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    
 }
