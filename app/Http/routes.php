6f72040998dd1d2dfc306fd768a658c3d490caf8
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-14 20:50:48 -0430

    agregadas rutas de proveedores y pruebas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,7 @@
 
 
 Route::group(['middleware' => ['web']], function () {
-
- route::get('/','VistaController@inicio');
+route::get('/','VistaController@inicio');
  route::get('registro','VistaController@registro');
  route::get('clientes','VistaController@clientes');
  route::get('proveedores','VistaController@proveedores');
@@ -33,8 +32,15 @@ Route::get('/buscarMunicipio/{id}/pertenece','VistaController@buscarMunicipios')
 
 Route::get('/buscarParroquia/{id}/pertenece','VistaController@buscarParroquias');
 
+Route::post('/inserta-proveedor','ProveedorController@insertar_proveedor' );
+Route::post('/inserta-materialprueba','PruebasController@insertar_matp' );
+
+Route::get('/lista-proveedor','ProveedorController@listar_proveedor' );
 
 });
+
+
+
 
 Route::auth();
 
