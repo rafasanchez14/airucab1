25ed1e8460ef2ad7e21649ce5e6ddc7b37635950
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-18 00:46:39 -0430

    crud pruebamat

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -10,10 +10,15 @@ class PersonalController extends Controller
 
   public function index()
   {
-      $lugares=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
-       $beneficiarios=DB::select("SELECT beneficiario.id_bene,beneficiario.nombre_bene,beneficiario.apelldido_bene,personal.id_personal||' '||personal.nombre_personal||' '||personal.apellido_personal as perso , l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais from beneficiario,personal,lugar l1,lugar l2,lugar l3,lugar l4 where beneficiario.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and (l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (beneficiario.cod_personal= personal.id_personal);");
-       $personal=DB::select("SELECT id_personal, id_personal||' '||nombre_personal||' '||apellido_personal as perso from personal;");
-      return view('portal/airucab-clientes',compact('lugares','clientes','personal','beneficiarios'));
+    $personal=DB::select(" SELECT personal.id_personal,personal.nombre_personal,personal.apellido_personal,personal.apellido2_personal,personal.nombre2_personal,
+personal.fechainicio,personal.fechafin, personal.experiencia,personal.titulacion, sede.nombre_sede,
+l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais
+from personal,sede,lugar l1,lugar l2,lugar l3,lugar l4 where personal.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and
+(l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (personal.cod_sede= sede.cod_sede);");
+   $sedes=DB::select("SELECT nombre_sede,cod_sede FROM sede  order by nombre_sede;");
+    $lugares=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
+
+    return view('portal/airucab-registro',compact('lugares','sedes','personal'));
 
   }
 public function insertar_pers(Request $request){
@@ -112,69 +117,48 @@ while ($i<$x)
   $i=$i+1;
 }
   }
+  $personal=DB::select(" SELECT personal.id_personal,personal.nombre_personal,personal.apellido_personal,personal.apellido2_personal,personal.nombre2_personal,
+  personal.fechainicio,personal.fechafin, personal.experiencia,personal.titulacion, sede.nombre_sede,
+  l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais
+  from personal,sede,lugar l1,lugar l2,lugar l3,lugar l4 where personal.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and
+  (l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (personal.cod_sede= sede.cod_sede);");
   $sedes=DB::select("SELECT nombre_sede,cod_sede FROM sede  order by nombre_sede;");
-   $lugares=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
-   $lugaresb=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
- return view('portal/airucab-registro',compact('lugares','sedes','lugaresb'));
+  $lugares=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
+
+  return view('portal/airucab-registro',compact('lugares','sedes','personal'));
 }
-public function insertar_bene(Request $request)
-{
-  $cedula=$request->cedula;
-  $nombre_bene=$request->nombre_bene;
-  $apellido_bene=$request->apellido_bene;
-  $telefono[]=$request->telefono;
-  $parroquia=$request->parroquia;
-  $codarea[]=$request->codarea;
-  $personal=$request->perso;
-  $telefono=$telefono[0];
-  $codarea=$codarea[0];
-  DB::insert('INSERT INTO beneficiario(
-            id_bene, nombre_bene, apelldido_bene, id_lugar, cod_personal)
-    VALUES (?, ?, ?, ?, ?)',[$cedula,$nombre_bene,$apellido_bene,$parroquia,$personal]);
-    $i=0;
-    $x=count($telefono);
-    if ($telefono[0]!='') {
-    while ($i<$x)
-        {
-    $codt=DB::select("SELECT cod_telf from telefono
-    order by cod_telf desc limit 1");
-            foreach ($codt as $key)
-            {
-              $ct=$key->cod_telf;
-            }
-            $ct=intval($ct);
-            $ct=$ct+1;
-            DB::insert('INSERT INTO telefono
-            (cod_telf,cod_area, numerotelf, id_bene)
-            VALUES (?,?, ?, ?)', [$ct,$codarea[$i],$telefono[$i],$cedula]);
-            $i=$i+1;
-        }
-    }
-    $personal=DB::select("SELECT id_personal, id_personal||' '||nombre_personal||' '||apellido_personal as perso from personal;");
-    $sedes=DB::select("SELECT nombre_sede,cod_sede FROM sede  order by nombre_sede;");
-     $lugares=DB::select("SELECT nombre_lugar,id_lugar FROM lugar WHERE tipo_lugar='pa' order by nombre_lugar;");
-   return view('portal/airucab-beneficiario',compact('lugares','sedes','personal'));
-  }
 
 
   public function edit($id)
   {
-      $id_bene=$id;
-      $lugares=DB::select("SELECT * from Lugar order by nombre_lugar;");
-      $beneficiarios=DB::select("SELECT beneficiario.id_bene,beneficiario.nombre_bene,beneficiario.apelldido_bene,personal.id_personal||' '||personal.nombre_personal||' '||personal.apellido_personal as perso , l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais from beneficiario,personal,lugar l1,lugar l2,lugar l3,lugar l4 where beneficiario.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and (l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (beneficiario.cod_personal= personal.id_personal);");
-       $bene=DB::selectOne("SELECT beneficiario.id_bene,beneficiario.nombre_bene,beneficiario.apelldido_bene,personal.id_personal||' '||personal.nombre_personal||' '||personal.apellido_personal as perso , l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais from beneficiario,personal,lugar l1,lugar l2,lugar l3,lugar l4 where beneficiario.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and (l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (beneficiario.cod_personal= personal.id_personal)and (beneficiario.id_bene=$id_bene);");
-       $telefono=DB::select("SELECT cod_telf,cod_area,numerotelf from telefono where id_bene=$id_bene");
+     $sedes=DB::select("SELECT nombre_sede,cod_sede FROM sede  order by nombre_sede;");
+      $id_personal=$id;
+       $lugares=DB::select("SELECT * from Lugar order by nombre_lugar;");
+      $personal=DB::select(" SELECT personal.id_personal,personal.nombre_personal,personal.apellido_personal,personal.apellido2_personal,personal.nombre2_personal,
+  personal.fechainicio,personal.fechafin, personal.experiencia,personal.titulacion, sede.nombre_sede,
+  l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais
+  from personal,sede,lugar l1,lugar l2,lugar l3,lugar l4 where personal.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and
+  (l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (personal.cod_sede= sede.cod_sede);");
+  $per=DB::selectOne(" SELECT personal.id_personal,personal.nombre_personal,personal.apellido_personal,personal.apellido2_personal,personal.nombre2_personal,
+personal.fechainicio,personal.fechafin, personal.experiencia,personal.titulacion, sede.nombre_sede,
+l1.nombre_lugar as parroquia,l2.nombre_lugar as municipio,l3.nombre_lugar as estado,l4.nombre_lugar as pais
+from personal,sede,lugar l1,lugar l2,lugar l3,lugar l4 where personal.id_lugar=l1.id_lugar and (l1.lugar_per=l2.id_lugar) and
+(l2.lugar_per=l3.id_Lugar) and (l3.lugar_per=l4.id_Lugar) and (personal.cod_sede= sede.cod_sede) and (personal.id_personal=$id_personal);");
+       $telefono=DB::select("SELECT cod_telf,cod_area,numerotelf from telefono where cod_personal=$id_personal");
+       $reds=DB::select("SELECT id_redsocial,plataforma,usuario from red_social where cod_personal=$id_personal");
 
-  return view('portal/airucab-beneficiariosEdit',compact('lugares','beneficiarios','bene','telefono'));
+  return view('portal/airucab-PersonalEdit',compact('lugares','sedes','personal','telefono','per','reds'));
   }
 
   public function update(Request $request, $id)
   {
-    DB::Update('UPDATE beneficiario
- SET  nombre_bene=?, apelldido_bene=?, id_lugar=?
-WHERE id_bene=?',array($request->nombre_bene,$request->apelldido_bene,$request->id_lugar,$id));
-Session::flash('save','Beneficiario modificado correctamente');
-return redirect('/beneficiarios');
+    DB::update("UPDATE personal
+       SET  nombre_personal=?, apellido_personal=?, nombre2_personal=?,
+           apellido2_personal=?, titulacion=?, fechainicio=?, fechafin=?,
+        id_lugar=?, cod_sede=? WHERE id_personal=? ",array($request->nombre_personal,$request->apellido_personal,$request->nombre2_personal,$request->apellido2_personal,
+         $request->titulacion,$request->fecha_ingr,$request->fecha_final,$request->id_lugar,$request->sede,$id));
+         Session::flash('save','Personal modificado correctamente');
+         return redirect('/clientes');
   }
 
   public function destroy($id)
@@ -183,19 +167,19 @@ return redirect('/beneficiarios');
   try {
 
       $data1=array($id);
-      DB::delete("DELETE  FROM telefono WHERE id_bene = ?",$data1);
-    DB::delete("DELETE   FROM beneficiario WHERE id_bene = ?",$data1);
-    Session::flash('save','Beneficiario eliminado correctamente');
-    return redirect('/beneficiarios');
-
+    DB::delete("DELETE  FROM telefono WHERE cod_personal = ?",$data1);
+    DB::delete("DELETE  FROM red_social WHERE cod_personal = ?",$data1);
+    DB::delete("DELETE   FROM personal WHERE id_personal = ?",$data1);
+    Session::flash('save','personal eliminado correctamente');
+    return redirect('/registro');
       }catch (\Illuminate\Database\QueryException $e){
-          Session::flash('delete','No es posible eliminar este beneficiario');
-    return redirect('/beneficiarios');
+          Session::flash('delete','No es posible eliminar este Empleado');
+    return redirect('/registro');
       }
 
   }
 
-  public function buscarB(Request $request)
+  public function buscarP(Request $request)
   {
 
       $nombre=$request->nombre;
