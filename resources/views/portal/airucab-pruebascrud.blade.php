6fe6deaaf2f19491892c08cf69e515a7cb95bc84
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-18 00:47:30 -0430

    crud pruebamat

---
+++ resources/views/portal/airucab-pruebascrud.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+@section('title','pruebas')
+@section('content')
+@include('layouts.messages')
+
+  <div class="tab-content " >
+  <div class="tab-pane fade in active" id="pruebascrud">
+
+<h1>HOLAS</h1>
+    </div></div>
