46ef6cc39f9e8fe8198affa3028eb56a99f02a73
Rafael Sanchez <rafaelsanchez814@gmail.com>
2017-12-19 04:59:29 -0430

    crud pruebas

--- resources/views/portal/airucab-pruebascrud.blade.php
+++ resources/views/portal/airucab-pruebascrud.blade.php
@@ -3,8 +3,76 @@
 @section('content')
 @include('layouts.messages')
 
-  <div class="tab-content " >
-  <div class="tab-pane fade in active" id="pruebascrud">
+<div class="container-fluid">
+  <div class="row">
+     <div class="col-lg-4 col-lg-offset-1 ">
+       <h2 id="margenSubmenu">PRUEBAS</h2>
+     </div>
+  </div>
 
-<h1>HOLAS</h1>
-    </div></div>
+</div>
+
+<div class="container " id="margenSubmenu">
+          <ul class="nav nav-tabs nav-justified ">
+            <li ><a id="Amarillo" href="/pruebascrud" >Pruebas</a></li>
+            <li class="active" ><a id="Verde" href="/pruebas" >Pruebas Materiales</a></li>
+          </ul>
+</div>
+
+<div class="tab-content " >
+  <div class="tab-pane fade in active" id="prueba">
+    <div class="container-fluid " id="margenSubmenu">
+<div class="row ">
+<div class="col-lg-4 col-lg-offset-4 " id="home">
+  <form class="" action="/inserta-prueba" method="post">
+    <div class="form-group">
+     <label for="formGroup">Nombre Prueba</label>
+      <input class="form-control"  name="nombre_prueb"type="text" placeholder="Ingrese la cédula*">
+  </div>
+  <div class="form-group">
+    <label for="comment">Descripcion:</label>
+    <textarea class="form-control" name="desc"rows="5" id="desc"></textarea>
+  </div>
+  <div class="row">
+      <div class="">
+        <div id="margenSubmenu">
+          <center><button id="botonAmarillo" type="submit" class="btn btn-default btn-block btn-lg"><span  class="fa fa-upload" aria-hidden="true"></span>  Agregar Prueba</button></center>
+        </form>
+        </div>
+      </div>
+  </div>
+
+
+</div>
+  </div>
+  </div>
+</div>
+</div>
+<div class="container-fluid tabla">
+  <div class="row">
+    <div class="col-lg-10 col-lg-offset-1 ">
+      <table class="table">
+        <thead id="botonAzul">
+          <th>Codigo</th>
+          <th>Fecha inicio</th>
+          <th>Fecha fin</th>
+          <th></th>
+          <th></th>
+        </thead>
+        @foreach($pruebas as $prueba)
+        <tbody class="well">
+          <td>{{$prueba->cod_prueba}}</td>
+          <td>{{$prueba->nombre_prueb}}</td>
+          <td>{{$prueba->descrip_prue}}</td>
+          <td>{!!link_to_route('pruebascrud.edit','Editar ',$parameters=$prueba->cod_prueba,$attributes=['class'=>'btn btn-success'])!!}
+          </td>
+          <td>{!!Form::open(['route'=>['pruebascrud.destroy',$prueba->cod_prueba],'method'=>'DELETE'])!!}
+            {!!Form::submit('Eliminar',['class'=>'btn btn-danger' ])!!}
+            {!!Form::close()!!}   </td>
+        </tbody>
+        @endforeach
+      </table>
+    </div>
+  </div>
+</div>
+@endsection
