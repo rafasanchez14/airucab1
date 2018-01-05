1cd996a9058fd3baa036e7ec6bfa93e765d85a83
Rafael Sanchez <rafaelsanchez814@gmail.com>
2018-01-05 06:10:54 -0400

    agregado reporte de modelo

--- app/Http/Controllers/reportController.php
+++ app/Http/Controllers/reportController.php
@@ -10,29 +10,37 @@ use DB;
 
 class reportController extends Controller
 {
-    
+
     public function index() {
 
-       
-   
+
+
     return view ('portal/airucab-Reportes');
-   
-   
+
+
     }
 
     public function proveedor(){
 
     $proveedores=DB::select(DB::raw( "SELECT p.id_proveedor as id,p.nombre,p.fechainic as fecha,p.montoac as monto,l.nombre_lugar as lugar
                                       from Proveedor p,Lugar l, Lugar l1
-                                      where p.id_lugar=l.id_lugar 
+                                      where p.id_lugar=l.id_lugar
                                       group by id,p.nombre,fecha,monto,lugar;"));
-    
-    
-     
+
+
+
     return view ('portal/airucab-listaP',compact('proveedores'));
 
-    
+
    }
+
+   public function modelo(){
+
+   $modelos=DB::select(DB::raw( "SELECT *
+                                     from modelo
+                                     order by id_modelo"));
+   return view ('portal/airucab-listaM',compact('modelos'));
+  }
 
 
 
